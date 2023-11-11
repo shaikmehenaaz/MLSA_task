be78d64954ef9cdbee8ba23378c485a4c722f641
Shaik Mehenaaz <mehenaaz.21bce7106@vitapstudent.ac.in>
2023-11-11 10:16:54 +0000

    added all modified files

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mehenaaz Shaik",
+  title: "Java Developer",
+  email: "mehenaaz.21bce7106@vitapstudent.ac.in",
+  gitHub: "https://github.com/shaikmehenaaz",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/mehenaaz/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Gray Scale to color image conversion",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Deep learning project working with GAN's and U-NET.",
+    url: "https://www.linkedin.com/posts/mehenaaz_image-colorization-using-gans-activity-7092067809504989184-_XaU/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Online food ordering system",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "whole documentation of how it is built and works.",
+    url: "https://www.linkedin.com/posts/mehenaaz_online-food-ordering-system-activity-7076283770680606720-dn5z/",
   },
   {
-    title: "My Resume Site",
+    title: "Chat application",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Java chat application using Swing and AWT",
+    url: "https://www.linkedin.com/posts/mehenaaz_java-awt-swing-activity-7118224179363004416-UI_V/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "UI-design",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "UI for art trading website.",
+    url: "https://www.linkedin.com/posts/mehenaaz_ui-activity-7074834909789310976-YrIh/",
   },
 ];
 
